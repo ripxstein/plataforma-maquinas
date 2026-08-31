0dfd3193523ae3057c7b5f33cc097a1e5820a9bb
Edson Alejandro <ripxstein@live.com.mx>
2026-08-31 14:48:31 -0600

    corregida configuracion de enlaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production') || request()->header('x-forwarded-proto') === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         View::composer('partials.sidebar-user', function ($view) {
             $modules = Module::with(['items' => function ($q) {
                 $q->orderBy('order');
